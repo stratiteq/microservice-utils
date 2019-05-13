0cf6b68eae9ffc0286bda111d60e8ffafe1cb10a
pulvinius <pulvinius@stratiteq.com>
2019-05-13 17:28:36 +0200

    Added WebApiClient package

---
+++ src/Stratiteq.Microservices.WebApiClient/AzureADConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stratiteq.Microservices.WebApiClient
+{
+    /// <summary>
+    /// Contains the information needed to make authenticated requests to a web API protected with Azure Active Directory (and role based authentication).
+    /// </summary>
+    public class AzureADConfiguration
+    {
+        /// <summary>
+        /// Gets or sets the tenant id of the Azure Active Directory (AAD) that hosts the application that is requesting access to another application.
+        /// The tenant id is the id of the AAD-instance. This is always a GUID.
+        /// </summary>
+        public string TenantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client id (aka application id) of the Azure Active Directory-application that is requesting access to another application. This is always a GUID.
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the subject name of the certificate that will be loaded and passed along the request to Azure Active Directory (AAD) to get an authentication token.
+        /// The certificate (without the private key, .cer format) must be uploaded to the AAD application itself so that it can verify the certificate.
+        /// The certificate (with the private key, pfx-format) must be uploaded to the web application host (App service or Azure Function).
+        /// </summary>
+        public string CertificateSubjectName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Azure Active Directory (AAD) application identifiers of the web APIs that the calling application needs authenticated access to.
+        /// These identifiers can be found in the AAD application settings, and is separate from the client id / application id. It has the form of a URI.
+        /// For the type parameter, use your typed HTTP client classes that inherit from WebApiClientBase.
+        /// </summary>
+        public Dictionary<Type, string> AADAppIdentifiers { get; set; } = new Dictionary<Type, string>();
+    }
+}
---
+++ src/Stratiteq.Microservices.WebApiClient/WebApiClientBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Stratiteq.Microservices.X509Certificate;
+
+namespace Stratiteq.Microservices.WebApiClient
+{
+    /// <summary>
+    /// Base class for creating typed HTTP Client classes that handles all authenticated machine-to-machine communication to a specific Web API.
+    /// Usage:
+    /// Inherit from this base type and then register the typed HTTP Client during startup (where you do your service configuration) with Services.AddHttpClient().
+    /// Also register a singleton instance of AzureADConfiguration, and add the typed HTTP client to its AADAppIdentifiers-collection.
+    /// </summary>
+    public abstract class WebApiClientBase
+    {
+        public const string DefaultScope = "/.default";
+
+        public WebApiClientBase(HttpClient httpClient, AzureADConfiguration azureADConfiguration)
+        {
+            HttpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            AzureADConfiguration = azureADConfiguration ?? throw new ArgumentNullException(nameof(azureADConfiguration));
+
+            ConfidentialClientApplication = ConfidentialClientApplicationBuilder
+                .Create(AzureADConfiguration.ClientId)
+                .WithCertificate(CertificateFinder.FindBySubjectName(azureADConfiguration.CertificateSubjectName, DateTime.UtcNow))
+                .WithAuthority(AzureCloudInstance.AzurePublic, AzureADConfiguration.TenantId)
+                .Build();
+
+            Scopes = new string[] { azureADConfiguration.AADAppIdentifiers[GetType()] + DefaultScope };
+        }
+
+        public HttpClient HttpClient { get; }
+
+        public AzureADConfiguration AzureADConfiguration { get; }
+
+        public IConfidentialClientApplication ConfidentialClientApplication { get; }
+
+        public string[] Scopes { get; }
+
+        protected async Task<AuthenticationHeaderValue> GetAuthHeaderValueAsync()
+        {
+            // TODO: Implement retries for when we get 429 som AAD:
+            // https://github.com/azureAD/microsoft-authentication-library-for-dotnet/wiki/retry-after
+            // Should use Polly instead for above example.
+            var result = await ConfidentialClientApplication.AcquireTokenForClient(Scopes).ExecuteAsync().ConfigureAwait(false);
+            return new AuthenticationHeaderValue("bearer", result.AccessToken);
+        }
+    }
+}
